84af4ff3b6d744b4ae4f1af17989208d9a1d7b6f
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-14 19:54:29 +0100

    Add the initial header file

---
+++ grimp-timetracker.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Grimp-TimeTracker
+Plugin URI: http://git.grimp.eu/
+Description: This plugin will allow you to manage hosting plans, hosting comparison etc.
+Version: 0.1
+Author: Fabio Alessandro Locati
+Author URI: http://grimp.eu
+License: GPL2
+*/
+
+?>
