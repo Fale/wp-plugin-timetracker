5e75d656a0e1eae5450adcb5a96ac7d209bdc1b1
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-17 11:13:18 +0100

    Insert types of hours

--- grimp-timetracker.php
+++ grimp-timetracker.php
@@ -17,6 +17,7 @@ add_action('admin_menu', 'grimp_timetracker_menu');
 function grimp_timetracker_menu() {
   add_menu_page(__('Timetracker','grimp-timetracker'), __('Timetracker','grimp-timetracker'), 'read', 'grimp-timetracker-options', 'grimp_timetracker_options');
   add_submenu_page( 'grimp-timetracker-options', __('Add Project','grimp-timetracker'), __('Add Project','grimp-timetracker'), 'manage_options', 'grimp-timetracker-add-project', 'grimp_timetracker_add_project');
+  add_submenu_page( 'grimp-timetracker-options', __('Add Type','grimp-timetracker'), __('Add Type','grimp-timetracker'), 'manage_options', 'grimp-timetracker-add-type', 'grimp_timetracker_add_type');
   add_submenu_page( 'grimp-timetracker-options', __('Add Hours','grimp-timetracker'), __('Add Hours','grimp-timetracker'), 'read', 'grimp-timetracker-add-hour', 'grimp_timetracker_add_hour');
 }
 
@@ -55,6 +56,32 @@ function grimp_timetracker_add_project() {
   echo $o;
 }
 
+function grimp_timetracker_add_type() {
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  global $wpdb;
+  
+  if(isset($_POST['submitted']) and $_POST['submitted'] == 'yes') {
+    $table_name = $wpdb->prefix . "timetracker_types";
+		$wpdb->insert( $table_name, array( 'id' => '', 'name' => $_POST['name'] ), array( '%i', '%s' ) );
+	}
+
+  $o = '<div class="wrap">';
+  $o.= '  <h2>Add a new type of hours:</h2>';
+  $o.= '  <form method="post" name="update_form" target="_self">';
+  $o.= '    Name <input type="text" name="name" value="" size="30" />';
+  $o.= '    <p class="submit" id="jump_submit">';
+  $o.= '      <input name="submitted" type="hidden" value="yes" />';
+  $o.= '      <input type="submit" name="Submit" value="Save Changes" />';
+  $o.= '    </p>';
+  $o.= '  </form>';
+  $o.= '</div>';
+  
+  echo $o;
+}
+
 function grimp_timetracker_add_hour() {
   if (!current_user_can('read'))  {
     wp_die( __('You do not have sufficient permissions to access this page.') );
@@ -64,15 +91,20 @@ function grimp_timetracker_add_hour() {
   global $user_ID;
   get_currentuserinfo();
 
-  $table_projects = $wpdb->prefix . "timetracker_projects";
   $table_hours = $wpdb->prefix . "timetracker_hours";
+  $table_projects = $wpdb->prefix . "timetracker_projects";
+  $table_types = $wpdb->prefix . "timetracker_types";
 
   if(isset($_POST['submitted']) and $_POST['submitted'] == 'yes')
 		$wpdb->insert( $table_projects, array( 'id' => '', 'person' => $user_ID, 'project' => $_POST['project'], 'hours' => $_POST['hours'], 'description' => $_POST['description'], 'day' => $_POST['day'] ), array( '%i', '%s', '%s', '%s', '%s', '%s' ) );
-  
+
   $ids = $wpdb->get_col("SELECT id FROM $table_projects");
   foreach($ids as $i => $id)
     $projects[] = $wpdb->get_row("SELECT * FROM $table_projects WHERE id = $id");
+
+  $ids = $wpdb->get_col("SELECT id FROM $table_types");
+  foreach($ids as $i => $id)
+    $types[] = $wpdb->get_row("SELECT * FROM $table_types WHERE id = $id");
 
   $o = '<div class="wrap">';
   $o.= '  <h2>Add a new hours:</h2>';
@@ -90,6 +122,13 @@ function grimp_timetracker_add_hour() {
   $o.= '        <td><input type="text" name="hours" value="1.00" size="30" /></td>';
   $o.= '      </tr>';
   $o.= '      <tr>';
+  $o.= '      <tr>';
+  $o.= '        <th>Project</th>';
+  $o.= '        <td><select name="type">';
+  foreach($types as $c => $type)
+    $o.= '          <option value="' . $type->name . '">' . $type->name . '</option>';
+  $o.= '        </select></td>';
+  $o.= '      </tr>';
   $o.= '        <th>Description</th>';
   $o.= '        <td><input type="text" name="description" value="" size="30" /></td>';
   $o.= '      </tr>';
