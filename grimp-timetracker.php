99f35164060c4d242446913638b0a76c3a274c24
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-17 09:14:27 +0100

    Add the pages I need in the administration

--- grimp-timetracker.php
+++ grimp-timetracker.php
@@ -12,4 +12,38 @@ License: GPL2
 include_once 'grimp-timetracker_setup.php';
 grimp_timetracker_setup();
 
+add_action('admin_menu', 'grimp_timetracker_menu');
+
+function grimp_timetracker_menu() {
+  add_menu_page(__('Timetracker','grimp-timetracker'), __('Timetracker','grimp-timetracker'), 'manage_options', 'grimp-timetracker-options', 'grimp_timetracker_options');
+  add_submenu_page( 'grimp-timetracker-options', __('Add Project','grimp-timetracker'), __('Add Project','grimp-timetracker'), 'manage_options', 'grimp-timetracker-add-project', 'grimp_timetracker_add_project');
+  add_submenu_page( 'grimp-timetracker-options', __('Add Hours','grimp-timetracker'), __('Add Hours','grimp-timetracker'), 'manage_options', 'grimp-timetracker-add-hour', 'grimp_timetracker_add_hour');
+}
+
+function grimp_timetracker_options() {
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+  
+  global $wpdb;
+  echo '<p>Here is where the form would go if I actually had options.</p>';
+}
+
+function grimp_timetracker_add_project() {
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  global $wpdb;
+  echo '<p>Here is where the form would go if I actually had options.</p>';
+}
+
+function grimp_timetracker_add_hour() {
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  global $wpdb;
+  echo '<p>Here is where the form would go if I actually had options.</p>';
+}
 ?>
