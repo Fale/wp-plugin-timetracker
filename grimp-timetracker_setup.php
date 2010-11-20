b2a4ddb688cc9a971019444d51bd1d8509d56507
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-20 12:46:38 +0100

    Version 0.4 will break database compatibility (id->ID)

--- grimp-timetracker_setup.php
+++ grimp-timetracker_setup.php
@@ -1,7 +1,7 @@
 <?php
 
 global $grimp_timetracker_db_version;
-$grimp_timetracker_db_version = "0.1";
+$grimp_timetracker_db_version = "0.2";
 
 function grimp_timetracker_setup() {
   global $wpdb;
@@ -10,7 +10,7 @@ function grimp_timetracker_setup() {
   $table_name = $wpdb->prefix . "timetracker_projects";
   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
     $projects = "CREATE TABLE " . $table_name . " (
-  	  id smallint unsigned NOT NULL AUTO_INCREMENT,
+  	  ID smallint unsigned NOT NULL AUTO_INCREMENT,
 	    name tinytext NOT NULL,
   	  UNIQUE KEY (id)
     	);";
@@ -24,7 +24,7 @@ function grimp_timetracker_setup() {
   $table_name = $wpdb->prefix . "timetracker_types";
   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
     $projects = "CREATE TABLE " . $table_name . " (
-  	  id smallint unsigned NOT NULL AUTO_INCREMENT,
+  	  ID smallint unsigned NOT NULL AUTO_INCREMENT,
 	    name tinytext NOT NULL,
   	  UNIQUE KEY (id)
     	);";
@@ -37,7 +37,7 @@ function grimp_timetracker_setup() {
   $table_name = $wpdb->prefix . "timetracker_hours";
   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
     $hours = "CREATE TABLE " . $table_name . " (
-  	  id smallint unsigned NOT NULL AUTO_INCREMENT,
+  	  ID smallint unsigned NOT NULL AUTO_INCREMENT,
  	    person smallint unsigned NOT NULL,
  	    project smallint unsigned NOT NULL,
 	    hours decimal(5,2) unsigned NOT NULL,
